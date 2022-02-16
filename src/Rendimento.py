714d5a51b6ea929fac5c0ebc408a3e6d604626ec
Samuel Nogueira <samuelnbacelar@hotmail.com>
2022-02-16 18:09:04 -0300

    Creating Rendimento Class

---
+++ src/Rendimento.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass, field
+from .exceptions.ValorRendimentoInvalidoException import ValorRendimentoInvalidoException
+from .exceptions.DescricaoEmBrancoException import DescricaoEmBrancoException
+
+
+@dataclass
+class Rendimento:
+    descricao: str
+    valor: float
+
+    def __post_init__(self):
+        if  type(self.valor) == str or self.valor == None or self.valor <= 0:
+            raise(ValorRendimentoInvalidoException)
+        if self.descricao == None or self.descricao == '':
+            raise(DescricaoEmBrancoException)
