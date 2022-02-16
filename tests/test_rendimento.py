0190f7a733ef3b63ef660e7e5a766e8adcb31973
Samuel Nogueira <samuelnbacelar@hotmail.com>
2022-02-16 17:35:50 -0300

    Adicionando testes de exceção

--- tests/test_rendimento.py
+++ tests/test_rendimento.py
@@ -4,11 +4,32 @@ from src.Rendimento import Rendimento
 
 rendimento_list = [
     ('rendimento de 100', 100),
-    ('rendimento de 0', 0),
-    ('rendimento de -1', -1),
+    ('rendimento de 1', 1),
+    ('rendimento de 999999', 999999),
 ]
 
-@pytest.mark.parametrize(['descricao', 'valor'], rendimento_list)
+rendimento_list_invalid_value = [
+    ('rendimento de 0', 0),
+    ('rendimento de -1', -1),
+    ('rendimento de ""', ''),
+    ('rendimento de None', None),
+]
+
+invalid_description_list = ['', None]
+
+@pytest.mark.parametrize(['descricao', 'valor'], rendimento_list_invalid_value)
 def test_rendimento_class(descricao, valor):
     um_rendimento = Rendimento(descricao, valor)
     assert um_rendimento.valor_rendimento == valor
+
+
+@pytest.mark.parametrize(['descricao', 'valor'], rendimento_list_invalid_value)
+def test_rendimento_class_invalid_value(descricao, valor):
+    with pytest.raises(ValorRendimentoInvalidoException):
+        Rendimento(descricao, valor)
+
+@pytest.mark.parametrize('descricao', invalid_description_list)
+def test_rendimento_class_invalid_description(descricao):
+    valor = 10
+    with pytest.raises(DescricaoEmBrancoException):
+        Rendimento(descricao, valor)
