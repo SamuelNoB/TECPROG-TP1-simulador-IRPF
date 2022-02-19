2c097a90ddb34a3cf0fcea7509808b1e63989e7a
Samuel Nogueira <samuelnbacelar@hotmail.com>
2022-02-19 13:03:29 -0300

    Adicionado testes para o controller

--- tests/test_rendimento_controller.py
+++ tests/test_rendimento_controller.py
@@ -1,5 +1,9 @@
+from unittest import result
 import pytest
+from src.models.Rendimento import Rendimento
 from src.controllers.RendimentoController import RendimentoController
+from src.exceptions.DescricaoEmBrancoException import DescricaoEmBrancoException
+from src.exceptions.ValorRendimentoInvalidoException import ValorRendimentoInvalidoException
 
 add_rendimento_list = [
     ('rendimento de 100', 100),
@@ -7,33 +11,52 @@ add_rendimento_list = [
     ('rendimento de 999999', 999999),
 ]
 
+rendimento_list_invalid_param = [
+    ('rendimento de 0', 0),
+    ('rendimento de -1', -1),
+    ('rendimento de ""', ''),
+    ('rendimento de None', None),
+    ('', None)
+]
+
+rendimento_list_count = [
+    ([('rend1', 10), ('rend2', 20)], 2),
+    ([('rend1', 10)], 1),
+    ([], 0)
+]
+
+rendimento_list_total_value = [
+    ([('rend1', 40), ('rend2', 40)], 80),
+    ([('rend1', 1000), ('rend1', 2000.25), ('rend1', 1000.45)], 4000.70),
+]
+
 @pytest.mark.parametrize(['descricao', 'valor'], add_rendimento_list)
 def test_add_rendimento(descricao, valor):
-    controller = RendimentoController()
+    controller = RendimentoController([])
     result = controller.add(valor, descricao)
     assert result.valor == valor
     assert result.descricao == descricao
 
-def test_add_invalid_rendimento():
-    ...
+@pytest.mark.parametrize(['descricao', 'valor'], rendimento_list_invalid_param)
+def test_add_invalid_rendimento(descricao, valor):
+    controller = RendimentoController()
+    with pytest.raises((ValorRendimentoInvalidoException, DescricaoEmBrancoException)):
+        controller.add(valor, descricao)
 
-def test_get_one_rendimento():
-    ...
+@pytest.mark.parametrize(['rendimentos', 'resultado'], rendimento_list_count)
+def test_list_rendimento(rendimentos, resultado):
+    controller = RendimentoController([])
+    for rendimento in rendimentos:
+        controller.add(rendimento[1], rendimento[0])
+    result = len(controller.list())
+    assert result == resultado
 
-def test_get_invalid_rendimento():
-    ...
-
-def test_list_rendimento():
-    ...
-
-def test_update_rendimento():
-    ...
-
-def test_update_invalid_rendimento():
-    ...
-
-def test_delete_rendimento():
-    ...
-
-def test_delete_invalid_rendimento():
-    ...
+@pytest.mark.parametrize(['rendimentos', 'resultado'], rendimento_list_total_value)
+def test_count_total_rendimento_value(rendimentos, resultado):
+    rendimentos_list = []
+    for rendimento in rendimentos:
+        rendimentos_list.append(Rendimento(rendimento[0], rendimento[1]))
+    
+    controller = RendimentoController(rendimentos_list)
+    result = controller.count_total_rendimento_value()
+    assert result == resultado
