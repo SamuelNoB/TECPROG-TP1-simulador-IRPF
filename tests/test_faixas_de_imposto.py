fca21f64de7a6fd8ea47ca7f9183cd7f30181d1c
Pedrok99 <pedrohlima20@gmail.com>
2022-02-21 14:48:00 -0300

    Duplicação
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>
    Co-authored-by: Sayuck <nobrega.m.roberto@gmail.com>

--- tests/test_faixas_de_imposto.py
+++ tests/test_faixas_de_imposto.py
@@ -3,8 +3,13 @@ from src.models.Faixas_de_imposto import Faixa_de_imposto
 
 rendimentos = [1, 10, 1000]
 
-faixa_1 = [
+faixa_um_valor = [
   (1500, 1500, 0),
+]
+
+faixa_dois_valores = [
+  (1000, 1000, 0),
+  (1903.98, 1903.98, 0),
 ]
 
 @pytest.mark.parametrize('rendimento', rendimentos)
@@ -12,10 +17,16 @@ def test_faixas_de_imposto_class(rendimento):
   faixa_de_imposto = Faixa_de_imposto(rbt=rendimento)
   assert faixa_de_imposto.rbt == rendimento
 
-@pytest.mark.parametrize(['rendimento', 'valor_base','imposto_esperado'], faixa_1)
-def test_faixa_1(rendimento, valor_base, imposto_esperado):
+@pytest.mark.parametrize(['rendimento', 'valor_base','imposto_esperado'], faixa_um_valor)
+def test_faixa_1_um_valor(rendimento, valor_base, imposto_esperado):
   faixa_de_imposto = Faixa_de_imposto(rbt=rendimento)
   imposto_calculado = faixa_de_imposto.calcular_imposto()
   assert imposto_calculado['faixa_1']['valor_imposto'] == imposto_esperado
   assert imposto_calculado['faixa_1']['valor_base'] == valor_base
 
+@pytest.mark.parametrize(['rendimento', 'valor_base','imposto_esperado'], faixa_dois_valores)
+def test_faixa_1_dois_valores(rendimento, valor_base, imposto_esperado):
+  faixa_de_imposto = Faixa_de_imposto(rbt=rendimento)
+  imposto_calculado = faixa_de_imposto.calcular_imposto()
+  assert imposto_calculado['faixa_1']['valor_imposto'] == imposto_esperado
+  assert imposto_calculado['faixa_1']['valor_base'] == valor_base
