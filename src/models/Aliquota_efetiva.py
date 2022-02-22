d9735138428e95f06c1c87d5b6e782c835cbad8b
Pedrok99 <pedrohlima20@gmail.com>
2022-02-22 15:07:00 -0300

    triangulação
    Co-authored-by: Matheus73 <matheuscoding@gmail.com>
    Co-authored-by: EduardoPicolo <eduardonpicolo@gmail.com>
    Co-authored-by: Sayuck <nobrega.m.roberto@gmail.com>

--- src/models/Aliquota_efetiva.py
+++ src/models/Aliquota_efetiva.py
@@ -1,9 +1,18 @@
 from dataclasses import dataclass
-
+import math
 @dataclass
 class Aliquota_efetiva:
   imposto_total:float
   valor_base:float
 
+  def __post_init__(self)->None:
+    def normal_round(n, decimals=0):
+      multiplier = 10 ** decimals
+      expoN = n * multiplier
+      if abs(expoN) - abs(math.floor(expoN)) < 0.5:
+        return math.floor(expoN) / multiplier
+      return math.ceil(expoN) / multiplier
+    self.normal_round = normal_round
+
   def calcular_aliquota(self) -> float:
-    return round((self.imposto_total / self.valor_base)*100 , 2)
+    return self.normal_round((self.imposto_total / self.valor_base)*100 , 2)
