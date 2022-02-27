4453cff3b2e65266301c50d8a5dbe320e9eb95b1
Pedrok99 <pedrohlima20@gmail.com>
2022-02-27 15:33:57 -0300

    Duplicação

--- src/models/Faixas_de_imposto.py
+++ src/models/Faixas_de_imposto.py
@@ -1,12 +1,75 @@
 from dataclasses import dataclass
+import math
 
 @dataclass
 class Faixa_de_imposto:
   rbt: float
+
+  def __post_init__(self) -> None:
+      self.tabela_irrf_2022 = {
+        'faixa_1':{
+          'min': 0,
+          'max':1903.98,
+          'aliquota': 0
+        },
+        'faixa_2':{
+          'min': 1903.99,
+          'max':2826.65,
+          'aliquota': 0.075
+        },
+        'faixa_3':{
+          'min': 2826.66,
+          'max':3751.05,
+          'aliquota': 0.15
+        },
+        'faixa_4':{
+          'min': 3751.06,
+          'max':4664.68,
+          'aliquota': 0.225
+        },
+        'faixa_5':{
+          'min': 4664.69,
+          'max':float('inf'),
+          'aliquota': 0.275
+        },
+      }
+      
+      def normal_round(n, decimals=0):
+        multiplier = 10 ** decimals
+        return math.ceil(n * multiplier) / multiplier
+      self.normal_round = normal_round
+ 
     
   def calcular_imposto(self) -> dict:
+    rendimento = self.rbt
+    calculado = {
+      'faixa_1': { 'valor_imposto': 0, 'valor_base': 0 },
+      'faixa_2': { 'valor_imposto': 0, 'valor_base': 0 },
+      'faixa_3': { 'valor_imposto': 0, 'valor_base': 0 },
+      'faixa_4': { 'valor_imposto': 0, 'valor_base': 0 },
+      'faixa_5': { 'valor_imposto': 0, 'valor_base': 0 },
+      'total':{'valor_imposto': 0, 'valor_base':0 }
+    }
 
-    return {'faixa_1':{
-      'valor_imposto': 0,
-      'valor_base':1500
-    }}
+
+    maximo_anterior = 0
+    for faixa in self.tabela_irrf_2022:
+
+      valor_maximo_na_faixa = round(self.tabela_irrf_2022[faixa]['max'] - maximo_anterior, 2)
+      print(maximo_anterior, valor_maximo_na_faixa)
+      maximo_anterior = self.tabela_irrf_2022[faixa]['max']
+
+      if(rendimento <= valor_maximo_na_faixa):
+        calculado[faixa]['valor_base'] = rendimento
+        calculado[faixa]['valor_imposto'] = self.normal_round(self.tabela_irrf_2022[faixa]['aliquota'] * rendimento, 4)
+        break
+        
+      else:
+        calculado[faixa]['valor_base'] = valor_maximo_na_faixa
+        rendimento = round(rendimento - calculado[faixa]['valor_base'], 2)
+        calculado[faixa]['valor_imposto'] = self.normal_round(
+          self.tabela_irrf_2022[faixa]['aliquota'] * valor_maximo_na_faixa, 4
+          )
+      
+    print(calculado)
+    return calculado
