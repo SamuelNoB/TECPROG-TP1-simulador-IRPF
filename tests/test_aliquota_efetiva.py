56d07e585c84f80780aae464d83147148a962286
Pedrok99 <pedrohlima20@gmail.com>
2022-02-28 15:01:26 -0300

    triangulação final

--- tests/test_aliquota_efetiva.py
+++ tests/test_aliquota_efetiva.py
@@ -8,20 +8,9 @@ dois_casos = [(7.20, 2000, 0.36), (376.37, 4500, 8.36)]
 
 tres_casos = [(0, 10, 0), (3255.64, 15000.00, 21.70), (0, 1900, 0)]
 
-@pytest.mark.parametrize(['imposto_total', 'valor_base', 'aliquota_efetiva_esperada'], um_caso)
-def test_calculo_aliquota_efetiva(imposto_total, valor_base, aliquota_efetiva_esperada):
-  aliquota_efetiva = Aliquota_efetiva(imposto_total, valor_base)
-  aliquota_calculada = aliquota_efetiva.calcular_aliquota()
-  assert aliquota_calculada == aliquota_efetiva_esperada
-
-@pytest.mark.parametrize(['imposto_total', 'valor_base', 'aliquota_efetiva_esperada'], dois_casos)
-def test_calculo_aliquota_efetiva(imposto_total, valor_base, aliquota_efetiva_esperada):
-  aliquota_efetiva = Aliquota_efetiva(imposto_total, valor_base)
-  aliquota_calculada = aliquota_efetiva.calcular_aliquota()
-  assert aliquota_calculada == aliquota_efetiva_esperada
-
-@pytest.mark.parametrize(['imposto_total', 'valor_base', 'aliquota_efetiva_esperada'], tres_casos)
-def test_calculo_aliquota_efetiva(imposto_total, valor_base, aliquota_efetiva_esperada):
-  aliquota_efetiva = Aliquota_efetiva(imposto_total, valor_base)
-  aliquota_calculada = aliquota_efetiva.calcular_aliquota()
-  assert aliquota_calculada == aliquota_efetiva_esperada
+@pytest.mark.parametrize('casos', [um_caso, dois_casos, tres_casos])
+def test_calculo_aliquota_efetiva(casos):
+    for imposto_total, valor_base, aliquota_efetiva_esperada in casos:
+      aliquota_efetiva = Aliquota_efetiva(imposto_total, valor_base)
+      aliquota_calculada = aliquota_efetiva.calcular_aliquota()
+      assert aliquota_calculada == aliquota_efetiva_esperada
