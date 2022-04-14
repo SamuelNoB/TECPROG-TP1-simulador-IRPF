3ed59c36d188d77f8acc98b4091ebb6cb1981e6e
Samuel Nogueira <samuelnbacelar@hotmail.com>
2022-04-13 21:31:10 -0300

    removendo metodo dentro de __post_init__

--- src/models/Aliquota_efetiva.py
+++ src/models/Aliquota_efetiva.py
@@ -5,14 +5,12 @@ class Aliquota_efetiva:
   imposto_total:float
   valor_base:float
 
-  def __post_init__(self)->None:
-    def normal_round(n, decimals=0):
-      multiplier = 10 ** decimals
-      expoN = n * multiplier
-      if abs(expoN) - abs(math.floor(expoN)) < 0.5:
-        return math.floor(expoN) / multiplier
-      return math.ceil(expoN) / multiplier
-    self.normal_round = normal_round
+  def normal_round(self, n, decimals=0):
+    multiplier = 10 ** decimals
+    expoN = n * multiplier
+    if abs(expoN) - abs(math.floor(expoN)) < 0.5:
+      return math.floor(expoN) / multiplier
+    return math.ceil(expoN) / multiplier
 
   def calcular_aliquota(self) -> float:
-    return self.normal_round((self.imposto_total / self.valor_base)*100 , 2)
+    return self.normal_round((self.imposto_total / self.valor_base)*100, 2)
